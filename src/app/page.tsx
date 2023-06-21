362e7053360170523679f53447f9694c58d42be2
MauPhu0906 <136775966+MauPhu0906@users.noreply.github.com>
2023-06-21 08:39:09 +0700

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+"use client"
+import Advantages from "@/components/Advantages/Advantages";
+import BusinessFeature from "@/components/BusinessFeature/BusinessFeature";
+import Contact from "@/components/Contact/Contact";
+import Footer from "@/components/Footer/Footer";
+import Hero from "@/components/Hero/Hero";
+import NavBar from "@/components/NavBar/NavBar";
+import Partner from "@/components/Partner/Partner";
+import Products from "@/components/Products/Products";
+import Solutions from "@/components/Solutions/Solutions";
+import { Carousel } from "flowbite";
+
+
+export default function Home() {
+  return (
+    <>
+      <NavBar/>
+      <Hero/>
+      <Products/>
+      <BusinessFeature/>
+      <Advantages/>
+      <Partner/>
+      <Solutions/>
+      <Contact/>
+      <Footer/>
+    </>
+  )
+}
